7534a2776131c2c9bd84c22b1797a22f3d44c3b6
Guilherme Nunes <guilherme.nunes@MacBook-Nunes.local>
2019-12-02 13:13:29 +0100

    Added GetTargetNodes streaming expression and it's tests

--- src/java/com/biologis/solr/client/solrj/io/stream/GetTargetNodesStream.java
+++ src/java/com/biologis/solr/client/solrj/io/stream/GetTargetNodesStream.java
@@ -2,51 +2,95 @@ package com.biologis.solr.client.solrj.io.stream;
 
 import org.apache.solr.client.solrj.io.Tuple;
 import org.apache.solr.client.solrj.io.comp.FieldComparator;
-import org.apache.solr.client.solrj.io.comp.MultipleFieldComparator;
 import org.apache.solr.client.solrj.io.comp.StreamComparator;
-import org.apache.solr.client.solrj.io.eq.FieldEqualitor;
-import org.apache.solr.client.solrj.io.eq.MultipleFieldEqualitor;
-import org.apache.solr.client.solrj.io.eq.StreamEqualitor;
 import org.apache.solr.client.solrj.io.stream.*;
 import org.apache.solr.client.solrj.io.stream.expr.*;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Locale;
+import java.util.*;
 
 public class GetTargetNodesStream extends TupleStream implements Expressible {
 
     private TupleStream stream;
-    private StreamFactory factory;
+    private StreamComparator comparator;
+    private GetTargetNodesStream.Worker worker;
+    private HashMap<String, String> options;
 
-    public GetTargetNodesStream(TupleStream stream) throws IOException{
-        this.init(stream, null);
-    }
 
-    public GetTargetNodesStream(StreamExpression expression, StreamFactory factory) throws IOException{
+    public GetTargetNodesStream(StreamExpression expression, StreamFactory factory) throws IOException {
         List<StreamExpression> streamExpressions = factory.getExpressionOperandsRepresentingTypes(expression, new Class[]{Expressible.class, TupleStream.class});
 
         if (expression.getParameters().size() != streamExpressions.size()) {
             throw new IOException(String.format(Locale.ROOT, "Invalid expression %s - unknown operands found", expression));
 
         } else if (streamExpressions.size() != 1) {
-            throw new IOException(String.format(Locale.ROOT, "Invalid expression %s - expecting two streams but found %d (must be TupleStream types)", expression, streamExpressions.size()));
+            throw new IOException(String.format(Locale.ROOT, "Invalid expression %s - expecting one stream but found %d (must be TupleStream types)", expression, streamExpressions.size()));
 
         } else {
-            this.init(factory.constructStream((StreamExpression)streamExpressions.get(0)), factory);
+            this.init(factory.constructStream(streamExpressions.get(0)), factory);
 
         }
     }
+
+
 
     public void init(TupleStream stream, StreamFactory factory) throws IOException{
         this.stream = stream;
-        this.factory = factory;
-        if (factory == null){
-            this.factory = new StreamFactory();
-        }
+        this.comparator = factory.constructComparator("its_endpoint_id asc", FieldComparator.class);
+        this.options = this.getSelectParameters();
+
+        worker = new GetTargetNodesStream.Worker() {
+
+            private LinkedList<Tuple> tuples = new LinkedList<Tuple>();
+            private Tuple eofTuple;
+
+            public void readStream(TupleStream stream) throws IOException {
+                HashSet<String> processedNodes = new HashSet<>();
+                Tuple tuple = stream.read();
+                String curNode;
+
+                while(!tuple.EOF){
+                    curNode = (String)tuple.get("node");
+
+                    if (!processedNodes.contains(curNode)){
+                        tuples.add(tuple);
+                        processedNodes.add(curNode);
+                    }
+
+                    tuple = stream.read();
+                }
+
+                eofTuple = tuple;
+            }
+
+            public void sort() {
+                tuples.sort(comparator);
+            }
+
+            public Tuple read() {
+                if(tuples.isEmpty()){
+                    return eofTuple;
+                }
+
+                return tuples.removeFirst();
+            }
+        };
+
+
     }
 
+    private HashMap<String, String> getSelectParameters(){
+        HashMap<String, String> options = new HashMap();
+
+        options.put("its_endpoint_id", "its_endpoint_id");
+        options.put("ss_endpoint_name", "ss_endpoint_name");
+        options.put("ss_endpoint_type", "ss_endpoint_type");
+        options.put("ss_search_api_id_container", "ss_search_api_id");
+        options.put("node", "node");
+        options.put("ancestors", "ancestors");
+
+        return options;
+    }
 
     @Override
     public void setStreamContext(StreamContext streamContext) {
@@ -60,48 +104,10 @@ public class GetTargetNodesStream extends TupleStream implements Expressible {
 
     @Override
     public void open() throws IOException {
-        //Sort by node asc
-        StreamComparator byNodeAsc = this.factory.constructComparator("node asc", FieldComparator.class);
-        SortStream innerSort = new SortStream(this.stream, byNodeAsc);
+        this.stream.open();
 
-        innerSort.open();
-
-        //Unique over node
-        UniqueStream unique = new UniqueStream(innerSort, this.convertToEqualitor(byNodeAsc));
-
-        //Select
-        //SelectStream select = new SelectStream(unique, this.getFieldList());
-
-    }
-
-    private String getSelectOptions(){
-        List<String> options = new ArrayList<String>();
-        options.add("endpoint_id");
-        options.add("endpoint_name");
-        options.add("endpoint_type");
-
-        options.add("endpoint_name");
-        options.add("endpoint_name");
-        options.add("endpoint_name");
-        options.add("endpoint_name");
-        options.add("endpoint_name");
-        return "";
-    }
-
-    private StreamEqualitor convertToEqualitor(StreamComparator comp) {
-        if (!(comp instanceof MultipleFieldComparator)) {
-            FieldComparator fComp = (FieldComparator)comp;
-            return new FieldEqualitor(fComp.getLeftFieldName(), fComp.getRightFieldName());
-        } else {
-            MultipleFieldComparator mComp = (MultipleFieldComparator)comp;
-            StreamEqualitor[] eqs = new StreamEqualitor[mComp.getComps().length];
-
-            for(int idx = 0; idx < mComp.getComps().length; ++idx) {
-                eqs[idx] = this.convertToEqualitor(mComp.getComps()[idx]);
-            }
-
-            return new MultipleFieldEqualitor(eqs);
-        }
+        worker.readStream(stream);
+        worker.sort();
     }
 
     @Override
@@ -111,12 +117,27 @@ public class GetTargetNodesStream extends TupleStream implements Expressible {
 
     @Override
     public Tuple read() throws IOException {
-        return null;
+        Tuple original = this.worker.read();
+
+        if(original.EOF){
+            return original;
+        }
+
+        Tuple result = new Tuple();
+
+        for(Object field : original.fields.keySet()){
+            if(this.options.containsKey(field)){
+                result.put(this.options.get(field), original.get(field));
+            }
+        }
+
+        return result;
+
     }
 
     @Override
     public StreamComparator getStreamSort() {
-        return this.stream.getStreamSort();
+        return comparator;
     }
 
     @Override
@@ -127,5 +148,13 @@ public class GetTargetNodesStream extends TupleStream implements Expressible {
     @Override
     public Explanation toExplanation(StreamFactory streamFactory) throws IOException {
         return this.stream.toExplanation(streamFactory);
+    }
+
+    private interface Worker {
+        void readStream(TupleStream var1) throws IOException;
+
+        void sort();
+
+        Tuple read();
     }
 }
---
+++ src/test/com/biologis/solr/client/solrj/io/stream/GetTargetNodesStreamTest.java
@@ -0,0 +1,39 @@
+package com.biologis.solr.client.solrj.io.stream;
+
+import junit.framework.TestCase;
+import org.apache.solr.client.solrj.io.Tuple;
+import org.junit.Assert;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.List;
+
+public class GetTargetNodesStreamTest extends TestCase {
+
+    private Util util;
+
+    @Override
+    protected void setUp() {
+        this.util = new Util();
+    }
+
+    @Test
+    public void test_compareWithOldCall() throws IOException {
+        String oldQueryString = "sort(select(unique(sort(select(search(kmm,q=*:*,fq=ss_direction:*,fl=\"its_endpoint_id,ss_endpoint_name,ss_endpoint_type,ss_endpoint_uuid,ss_to_endpoint_uuid,ss_to_link_bag_uuid,ss_to_link_uuid,its_num_links,ss_direction,ss_conjunction,ss_search_api_id_container\",sort=\"ss_to_endpoint_uuid ASC\",qt=\"/export\"),boost_document,ss_conjunction,its_endpoint_id,ss_endpoint_name,ss_endpoint_type,ss_endpoint_uuid,its_evaluation_id,ss_evaluation_name,ss_evaluation_of_uuid,ss_evaluation_uuid,its_link_id,ss_link_type,ss_link_uuid,ss_metadata_flags,its_metadata_id,ss_metadata_name,ss_metadata_name_md5,ss_metadata_of_uuid,ss_metadata_type,ss_metadata_uuid,bs_status,ss_to_endpoint_uuid,ss_to_link_bag_uuid,ss_search_api_id,ss_search_api_datasource,ss_search_api_language,ss_direction,ss_to_link_uuid,its_num_links,ss_search_api_id_container,id,index_id,hash,site,timestamp,sm_context_tags,spell,sort_X3b_und_search_api_relevance,sort_X3b_und_conjunction,sort_X3b_und_endpoint_name,sort_X3b_und_endpoint_type,sort_X3b_und_endpoint_uuid,sort_X3b_und_evaluation_name,sort_X3b_und_evaluation_of_uuid,sort_X3b_und_evaluation_uuid,sort_X3b_und_link_type,sort_X3b_und_link_uuid,sort_X3b_und_metadata_flags,sort_X3b_und_metadata_name,sort_X3b_und_metadata_name_md5,sort_X3b_und_metadata_of_uuid,sort_X3b_und_metadata_type,sort_X3b_und_metadata_uuid,sort_X3b_und_to_endpoint_uuid,sort_X3b_und_to_link_bag_uuid,sort_X3b_und_search_api_id,sort_X3b_und_search_api_datasource,sort_X3b_und_search_api_language,sort_X3b_und_direction,sort_X3b_und_to_link_uuid,sort_X3b_und_search_api_id_container,sort_X3b_und_site,sort_X3b_und_timestamp,sort_X3b_und_context_tags,sort_X3b_und_spell,concat(fields=\"ss_endpoint_uuid\",delim=\",\",as=\"node\")),by=\"node asc\"),over=node),its_endpoint_id,ss_endpoint_name,ss_endpoint_type,ss_search_api_id_container as ss_search_api_id,node,ancestors),by=\"its_endpoint_id asc\")";
+        String newQueryString = "getTargetNodes(select(search(kmm,q=*:*,fq=ss_direction:*,fl=\"its_endpoint_id,ss_endpoint_name,ss_endpoint_type,ss_endpoint_uuid,ss_to_endpoint_uuid,ss_to_link_bag_uuid,ss_to_link_uuid,its_num_links,ss_direction,ss_conjunction,ss_search_api_id_container\",sort=\"ss_to_endpoint_uuid ASC\",qt=\"/export\"),boost_document,ss_conjunction,its_endpoint_id,ss_endpoint_name,ss_endpoint_type,ss_endpoint_uuid,its_evaluation_id,ss_evaluation_name,ss_evaluation_of_uuid,ss_evaluation_uuid,its_link_id,ss_link_type,ss_link_uuid,ss_metadata_flags,its_metadata_id,ss_metadata_name,ss_metadata_name_md5,ss_metadata_of_uuid,ss_metadata_type,ss_metadata_uuid,bs_status,ss_to_endpoint_uuid,ss_to_link_bag_uuid,ss_search_api_id,ss_search_api_datasource,ss_search_api_language,ss_direction,ss_to_link_uuid,its_num_links,ss_search_api_id_container,id,index_id,hash,site,timestamp,sm_context_tags,spell,sort_X3b_und_search_api_relevance,sort_X3b_und_conjunction,sort_X3b_und_endpoint_name,sort_X3b_und_endpoint_type,sort_X3b_und_endpoint_uuid,sort_X3b_und_evaluation_name,sort_X3b_und_evaluation_of_uuid,sort_X3b_und_evaluation_uuid,sort_X3b_und_link_type,sort_X3b_und_link_uuid,sort_X3b_und_metadata_flags,sort_X3b_und_metadata_name,sort_X3b_und_metadata_name_md5,sort_X3b_und_metadata_of_uuid,sort_X3b_und_metadata_type,sort_X3b_und_metadata_uuid,sort_X3b_und_to_endpoint_uuid,sort_X3b_und_to_link_bag_uuid,sort_X3b_und_search_api_id,sort_X3b_und_search_api_datasource,sort_X3b_und_search_api_language,sort_X3b_und_direction,sort_X3b_und_to_link_uuid,sort_X3b_und_search_api_id_container,sort_X3b_und_site,sort_X3b_und_timestamp,sort_X3b_und_context_tags,sort_X3b_und_spell,concat(fields=\"ss_endpoint_uuid\",delim=\",\",as=\"node\")))";
+
+        List<Tuple> oldTuples = this.util.getTuples(oldQueryString);
+        List<Tuple> newTuples = this.util.getTuples(newQueryString);
+
+        Assert.assertTrue(oldTuples.size() > 0);
+
+        Assert.assertEquals(oldTuples.size(), newTuples.size());
+
+        for (int i = 0; i < oldTuples.size(); i++) {
+            Assert.assertTrue(this.util.areTuplesEqual(oldTuples.get(i), newTuples.get(i)));
+        }
+
+    }
+
+}
